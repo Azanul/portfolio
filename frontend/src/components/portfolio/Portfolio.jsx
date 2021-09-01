e364512f8486be3664566d05d159fd380c8dbf5b
Ambareen09 <64471630+Ambareen09@users.noreply.github.com>
2021-09-01 23:46:23 +0530

    portfolio, testimonals icons added and linked

--- frontend/src/components/portfolio/Portfolio.jsx
+++ frontend/src/components/portfolio/Portfolio.jsx
@@ -53,10 +53,12 @@ export default function Portfolio() {
       <div className="container">
         {data.map((d) => (
           <div className="item">
+              <a href={d.link}>
             <img
               src={d.img}
               alt=""
             />
+              </a>
             <h3>{d.title}</h3>
           </div>
         ))}
--- frontend/src/components/testimonials/Testimonials.jsx
+++ frontend/src/components/testimonials/Testimonials.jsx
@@ -4,38 +4,48 @@ import React from "react";
 export default function Testimonials(props) {
   const data = [{
       id: 1,
-      name: "Tom Durden",
-      img: "https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      name: "Twitter",
+      img: "assets/twitterr.png",
+      link: "https://twitter.com/AzanulZ",
     },
     {
       id: 2,
-      name: "Tom Durden",
-      img: "https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      name: "Instagram",
+      img: "assets/Instagram_logo.png",
+      link: "https://www.instagram.com/john.doe_297/",
+
     },
     {
       id: 3,
-      name: "Tom Durden",
-      img: "https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      name: "facebook",
+      img: "assets/fb.png",
+      link: "https://www.facebook.com/azanul.haque/",
+
     },
     {
       id: 4,
-      name: "Tom Durden",
-      img: "https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      name: "CodeChef",
+      img: "assets/codechef.png",
+      link: "https://www.codechef.com/users/johndoe_297",
+
     },
     {
       id: 5,
-      name: "Alex Kalinski",
-      img: "https://images.pexels.com/photos/428321/pexels-photo-428321.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      name: "LinkedIn",
+      img: "assets/linkedinn.png",
+      link: "https://www.linkedin.com/in/azanul-haque/",
+
       featured: true,
     },
     {
       id: 6,
-      name: "Martin Harold",
-      img: "https://images.pexels.com/photos/3863793/pexels-photo-3863793.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      name: "Codeforces",
+      img: "assets/codeforces.jpg",
+      link: "https://codeforces.com/profile/JohnDoe_297",
     },
     {
       id: 7,
-      name: "Martin Harold",
+      name: "Codingame",
       img: "https://images.pexels.com/photos/3863793/pexels-photo-3863793.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
     },
     {
@@ -58,11 +68,13 @@ export default function Testimonials(props) {
             {data.map((d) => (
                 <div className={d.featured ? "card featured" : "card"}>
                   <div className="top">
+                    <a href={d.link}>
                     <img
                         className="user"
                         src={d.img}
                         alt=""
                     />
+                    </a>
                   </div>
                 </div>
             ))}
